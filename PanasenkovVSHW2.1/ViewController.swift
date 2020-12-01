6719bb6783b2f17d816152ccf0b682ad40c5c288
Vladimir <p-bobka@mail.ru>
2020-12-02 00:03:03 +0300

    Initial Commit

---
+++ PanasenkovVSHW2.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PanasenkovVSHW2.1
+//
+//  Created by Владимир Панасенков on 01.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
